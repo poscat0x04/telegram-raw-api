ed9ae6c010cd1deffe5497654e44ec0f64f673dd
Poscat <poscat@mail.poscat.moe>
2020-04-04 16:17:02 +0800

    Update telegram-types source

--- nix/telegram-types.nix
+++ nix/telegram-types.nix
@@ -5,7 +5,12 @@
 mkDerivation {
   pname = "telegram-types";
   version = "0.1.0.0";
-  src = ../../telegram-types;
+  src = fetchgit {
+    url = "https://github.com/poscat0x04/telegram-types";
+    sha256 = "1qwx71qn5kmaw52kfvwvx6xsd8799cwpv8bcg5sfhvx6cibvsxpc";
+    rev = "e09187766d7c27a575fb881fe26fd6a9c9c3a67e";
+    fetchSubmodules = true;
+  };
   libraryHaskellDepends = [
     aeson base bytestring deriving-aeson filepath mime-types open-union
     servant servant-multipart text unordered-containers
