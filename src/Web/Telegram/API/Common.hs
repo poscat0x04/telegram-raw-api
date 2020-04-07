db11e8e2813bd46fb3a0eef78281f1b10cba7555
Poscat <poscat@mail.poscat.moe>
2020-04-07 21:36:42 +0800

    Derive instances for Token

--- src/Web/Telegram/API/Common.hs
+++ src/Web/Telegram/API/Common.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Web.Telegram.API.Common where
 
 import Data.Text (Text)
+import GHC.Generics
 import Servant.API
 
 newtype Token = Token Text
+  deriving (Show, Eq, Generic)
 
 instance ToHttpApiData Token where
   toQueryParam (Token t) = "bot" <> t
