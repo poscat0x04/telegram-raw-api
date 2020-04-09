e3f5100d1ee48a8b86b899ffb3eb1fb82ab0d91f
Poscat <poscat@mail.poscat.moe>
2020-04-09 15:33:39 +0800

    Derive IsString, ToJSON, FromJSON for Token

--- src/Web/Telegram/API/Common.hs
+++ src/Web/Telegram/API/Common.hs
@@ -1,15 +1,20 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Web.Telegram.API.Common where
 
 import Data.Text (Text)
 import GHC.Generics
 import Servant.API
+import Data.Aeson
+import Data.String
 
 newtype Token = Token Text
   deriving (Show, Eq, Generic)
+  deriving newtype (IsString, ToJSON, FromJSON)
 
 instance ToHttpApiData Token where
   toQueryParam (Token t) = "bot" <> t
