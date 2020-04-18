c5126faebea3f63430c57af81b354658f58418b4
Poscat <poscat@mail.poscat.moe>
2020-04-18 22:33:04 +0800

    Update dependency

--- nix/telegram-types.nix
+++ nix/telegram-types.nix
@@ -1,31 +1,23 @@
-{ mkDerivation, aeson, base, bytestring, deriving-aeson, fetchgit
-, filepath, generic-lens, hpack, lens, mime-types, open-union
-, servant, servant-multipart, stdenv, text, unordered-containers
+{ mkDerivation, aeson, base, bytestring, deriving-aeson, filepath
+, generic-lens, lens, mime-types, open-union, servant
+, servant-multipart, stdenv, text
 }:
 mkDerivation {
   pname = "telegram-types";
-  version = "0.1.0.0";
-  src = fetchgit {
-    url = "https://github.com/poscat0x04/telegram-types";
-    sha256 = "0ipzsdgs4m8wn1ifajycxn98zb3d8kfz1axhw5gvzjvb7ijrblsc";
-    rev = "26aa9434bd96068aded732a99e985dad4c2ba649";
-    fetchSubmodules = false;
-  };
+  version = "0.2.0";
+  sha256 = "94c4509a967ae4778c6a4121e699f2139da0b984ce552357d16b9f102a36d7ec";
   libraryHaskellDepends = [
     aeson base bytestring deriving-aeson filepath generic-lens lens
     mime-types open-union servant servant-multipart text
-    unordered-containers
   ];
-  libraryToolDepends = [ hpack ];
   testHaskellDepends = [
     aeson base bytestring deriving-aeson filepath generic-lens lens
     mime-types open-union servant servant-multipart text
-    unordered-containers
   ];
   enableLibraryProfiling = false;
   enableExecutableProfiling = false;
   doCheck = false;
-  prePatch = "hpack";
   homepage = "https://github.com/poscat0x04/telegram-types#readme";
+  description = "Types used in Telegram bot API";
   license = stdenv.lib.licenses.bsd3;
 }
